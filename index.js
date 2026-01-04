9aba2258bb15f507a3fd5d20ce3631209df61763
Northen-Light <prateekpatnaikcomp@gmail.com>
2026-01-04 16:32:02 +0530

    draining-micro-task-queue-example

---
+++ index.js
@@ -0,0 +1,21 @@
+// unless you use src tag in script, defer won't work
+// defer don't work on inline scripts
+
+setTimeout(() => {
+    console.log('macro-task-queue-cb-1');
+    queueMicrotask(() => console.log('micro-task-queue-cb-100'));
+}, 0);
+
+setTimeout(() => console.log('macro-task-queue-cb-2'), 0);
+
+/*
+* Draining microtasks amortizes the native-to-JS entry cost,
+* improves cache locality, prevents stack blowups from recursive
+* microtasks, and enforces deterministic scheduling — all of which
+* significantly improve performance.
+* */
+for (let i = 0; i < 1000; i++) queueMicrotask(() => console.log(`micro-task-queue-cb-${i}`));
+
+const a = new Array(100000).fill(1000);
+
+console.log(a);
